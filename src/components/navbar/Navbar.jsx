dff9f79b3f3a8b64045d6227344e84e233ca2747
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-06-20 23:03:50 -0600

    feat: navbar sm screen hamberger

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import "./navbar.scss";
 
 function Navbar() {
+  const [open, setOpen] = useState(false);
   return (
     <nav>
       <div className="left">
@@ -18,6 +20,17 @@ function Navbar() {
         <a href="/" className="register">
           Sign up
         </a>
+        <div className="menuIcon">
+          <img src="menu.png" alt="" onClick={() => setOpen(!open)} />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign in</a>
+          <a href="/">Sign up</a>
+        </div>
       </div>
     </nav>
   );
