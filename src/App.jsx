1104db5a39138b020ced2ac7cbee889d4063af5c
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-06-20 23:49:58 -0600

    feat: search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Navbar from "./components/navbar/Navbar";
 import "./layout.scss";
+import { HomePage } from "./pages/HomePage";
 
 function App() {
   return (
     <div className="layout">
-      <Navbar />
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="content">
+        <HomePage />
+      </div>
     </div>
   );
 }
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,39 @@
+import { SearchBar } from "../components/searchBar/SearchBar";
+import "./homePage.scss";
+
+export const HomePage = () => {
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+            {" "}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+            aliquid rem, ex distinctio soluta itaque iusto! Omnis amet atque,
+            quos dolorem ut eveniet accusantium in veritatis iusto harum minima
+            perferendis?
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>1200+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="bg.png" alt="" />
+      </div>
+    </div>
+  );
+};
