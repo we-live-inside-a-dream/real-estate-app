fc7f5bde23fc5f586314bdebcf2e49d9d411957a
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-06-22 13:52:48 -0600

    feat: singlePage details section

---
+++ src/components/slider/Slider.jsx
@@ -0,0 +1,5 @@
+import "./slider.scss";
+
+export const Slider = () => {
+  return <div className="slider">Slider</div>;
+};
--- src/pages/singlePage/SinglePage.jsx
+++ src/pages/singlePage/SinglePage.jsx
@@ -1,5 +1,35 @@
+import { Slider } from "../../components/slider/Slider";
 import "./singlePage.scss";
+import { singlePostData, userData } from "../../lib/dummydata";
 
 export const SinglePage = () => {
-  return <div>single</div>;
+  return (
+    <div className="singlePage">
+      <div className="details">
+        <div className="wrapper">
+          <Slider images={singlePostData.images} />
+          <div className="info">
+            <div className="top">
+              <div className="post">
+                <h1>{singlePostData.title}</h1>
+                <div className="address">
+                  <img src="/pin.png" alt="" />
+                  <span>{singlePostData.address}</span>
+                </div>
+                <div className="price">$ {singlePostData.price}</div>
+              </div>
+              <div className="user">
+                <img src={userData.img} alt="" />
+                <span>{userData.name}</span>
+              </div>
+            </div>
+            <div className="bottom">Bottom</div>
+          </div>
+        </div>
+      </div>
+      <div className="features">
+        <div className="wrapper"></div>
+      </div>
+    </div>
+  );
 };
