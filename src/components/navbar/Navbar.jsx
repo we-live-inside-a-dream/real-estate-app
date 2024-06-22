f527e3d44ec593c4c22296b3e66a793b7475adda
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-06-22 00:19:49 -0600

    feath: card list

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { useState } from "react";
 import "./navbar.scss";
 
@@ -6,30 +7,30 @@ function Navbar() {
   return (
     <nav>
       <div className="left">
-        <a href="/" className="logo">
+        <Link to={"/"} className="logo">
           <img src="/logo.png" alt="" />
           <span>LamaEstate</span>
-        </a>
-        <a href="/">Home</a>
-        <a href="/">About</a>
-        <a href="/">Contact</a>
-        <a href="/">Agents</a>
+        </Link>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/"}>About</Link>
+        <Link to={"/"}>Contact</Link>
+        <Link to={"/"}>Agents</Link>
       </div>
       <div className="right">
-        <a href="/">Sign in</a>
-        <a href="/" className="register">
+        <Link to={"/"}>Sign in</Link>
+        <Link to={"/"} className="register">
           Sign up
-        </a>
+        </Link>
         <div className="menuIcon">
           <img src="menu.png" alt="" onClick={() => setOpen(!open)} />
         </div>
         <div className={open ? "menu active" : "menu"}>
-          <a href="/">Home</a>
-          <a href="/">About</a>
-          <a href="/">Contact</a>
-          <a href="/">Agents</a>
-          <a href="/">Sign in</a>
-          <a href="/">Sign up</a>
+          <Link to={"/"}>Home</Link>
+          <Link to={"/"}>About</Link>
+          <Link to={"/"}>Contact</Link>
+          <Link to={"/"}>Agents</Link>
+          <Link to={"/"}>Sign in</Link>
+          <Link to={"/"}>Sign up</Link>
         </div>
       </div>
     </nav>
