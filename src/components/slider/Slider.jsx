460eb417480051a6d7a213502acd9f6d653b88cc
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-06-23 20:25:27 -0600

    feat: full slider

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,14 +1,59 @@
+import { useState } from "react";
 import "./slider.scss";
 
 export const Slider = ({ images }) => {
+  const [imageIndex, setImageIndex] = useState(null);
+
+  const changeSlide = (direction) => {
+    if (direction === "left") {
+      if (imageIndex === 0) {
+        setImageIndex(images.length - 1);
+      } else {
+        setImageIndex(imageIndex - 1);
+      }
+    } else {
+      if (imageIndex === images.length - 1) {
+        setImageIndex(0);
+      } else {
+        setImageIndex(imageIndex + 1);
+      }
+    }
+  };
+
   return (
     <div className="slider">
+      {imageIndex !== null && (
+        <div className="fullSlider">
+          <div className="arrow">
+            <img src="/arrow.png" alt="" onClick={() => changeSlide("left")} />
+          </div>
+          <div className="imageContainer">
+            <img src={images[imageIndex]} alt="" />
+          </div>
+          <div className="arrow">
+            <img
+              src="/arrow.png"
+              alt=""
+              className="right"
+              onClick={() => changeSlide("right")}
+            />
+          </div>
+          <div className="close" onClick={() => setImageIndex(null)}>
+            X
+          </div>
+        </div>
+      )}
       <div className="bigImage">
-        <img src={images[0]} alt="" />
+        <img src={images[0]} alt="" onClick={() => setImageIndex(0)} />
       </div>
       <div className="smallImage">
         {images.slice(1).map((image, index) => (
-          <img src={image} alt="" key={index} />
+          <img
+            src={image}
+            alt=""
+            key={index}
+            onClick={() => setImageIndex(index + 1)}
+          />
         ))}
       </div>
     </div>
