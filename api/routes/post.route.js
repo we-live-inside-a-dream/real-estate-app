3168ed787ea5f29f427e0bcfe763e573a79916b8
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-07-01 21:56:54 -0600

    feat: authorization test api

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -2,17 +2,17 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/test", (req, res) => {
-  console.log("router works!");
-});
-router.post("/test", (req, res) => {
-  console.log("router works!");
-});
-router.put("/test", (req, res) => {
-  console.log("router works!");
-});
-router.delete("/test", (req, res) => {
-  console.log("router works!");
-});
+// router.get("/test", (req, res) => {
+//   console.log("router works!");
+// });
+// router.post("/test", (req, res) => {
+//   console.log("router works!");
+// });
+// router.put("/test", (req, res) => {
+//   console.log("router works!");
+// });
+// router.delete("/test", (req, res) => {
+//   console.log("router works!");
+// });
 
 export default router;
