82f90649669fa379fdd797cc3c86cd9094f8636c
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-07-21 09:54:26 -0600

    feat: get chat and add chat controller

--- api/controllers/chat.controller.js
+++ api/controllers/chat.controller.js
@@ -1,15 +1,15 @@
 import prisma from "../lib/prisma.js";
 
 export const getChats = async (req, res) => {
-    const tokenUserId = req.userId;
+  const tokenUserId = req.userId;
   try {
     const chats = await prisma.chat.findMany({
-        where:{
-            userIDs:{
-                hasSome: [tokenUserId],
-            }
-        }
-    })
+      where: {
+        userIDs: {
+          hasSome: [tokenUserId],
+        },
+      },
+    });
     res.status(200).json(chats);
   } catch (err) {
     console.log(err);
@@ -18,20 +18,35 @@ export const getChats = async (req, res) => {
 };
 
 export const getChat = async (req, res) => {
+  const tokenUserId = req.userId;
   try {
-    res.status(200).json();
+    const chat = await prisma.chat.findUnique({
+      where: {
+        id: req.params.id,
+        userIDs: {
+          hasSome: [tokenUserId],
+        },
+      },
+    });
+    res.status(200).json(chat);
   } catch (err) {
     console.log(err);
-    res.status(500).json({ message: "Failed to get Chats!" });
+    res.status(500).json({ message: "Failed to get Chat!" });
   }
 };
 
 export const addChat = async (req, res) => {
+  const tokenUserId = req.userId;
   try {
-    res.status(200).json();
+    const newChat = await prisma.chat.create({
+      data: {
+        userIDs: [tokenUserId, req.body.receiverId],
+      },
+    });
+    res.status(200).json(newChat);
   } catch (err) {
     console.log(err);
-    res.status(500).json({ message: "Failed to get Chats!" });
+    res.status(500).json({ message: "Failed to add Chat!" });
   }
 };
 
@@ -40,6 +55,6 @@ export const readChat = async (req, res) => {
     res.status(200).json();
   } catch (err) {
     console.log(err);
-    res.status(500).json({ message: "Failed to get Chats!" });
+    res.status(500).json({ message: "Failed to read Chat!" });
   }
 };
