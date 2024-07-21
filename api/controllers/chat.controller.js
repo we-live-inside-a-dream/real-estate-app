460b7fca449db2f2025e3e40c6b048d541896c1d
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-07-21 09:45:27 -0600

    fix: import

--- api/controllers/chat.controller.js
+++ api/controllers/chat.controller.js
@@ -1,4 +1,4 @@
-import prisma from "../lib/prisma";
+import prisma from "../lib/prisma.js";
 
 export const getChats = async (req, res) => {
     const tokenUserId = req.userId;
--- api/controllers/message.controller.js
+++ api/controllers/message.controller.js
@@ -1,4 +1,4 @@
-import prisma from "../lib/prisma";
+import prisma from "../lib/prisma.js";
 
 export const addMessage = async (req, res) => {
   try {
