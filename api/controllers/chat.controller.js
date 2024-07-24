3d57180b1c76154fe4e0937cd7a12fae8d23d6a4
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-07-24 13:53:04 -0600

    feat: chat system front end

--- api/controllers/chat.controller.js
+++ api/controllers/chat.controller.js
@@ -10,6 +10,23 @@ export const getChats = async (req, res) => {
         },
       },
     });
+
+    for(const chat of chats) {
+        const receiverId = chat.userIDs.find(id => id !== tokenUserId);
+
+        const receiver = await prisma.user.findUnique({
+            where:{
+                id: receiverId
+            },
+            select:{
+                id: true,
+                username: true,
+                avatar: true,
+            },
+        });
+        chat.receiver = receiver;
+    }
+
     res.status(200).json(chats);
   } catch (err) {
     console.log(err);
