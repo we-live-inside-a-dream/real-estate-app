28b4481f9e3a7b312280fc1cfa6d63fd0fe913b6
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-07-26 15:54:40 -0600

    script change

--- api/client/src/lib/apiRequest.js
+++ api/client/src/lib/apiRequest.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const apiRequest = axios
   .create({
-    baseURL: "http://localhost:8800/api",
+    // baseURL: "http://localhost:8800/api",
+    baseURL: "https://realestate2023-a4c53d4146f3.herokuapp.com/api/",
     withCredentials: true,
   })
 
